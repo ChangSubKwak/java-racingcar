68b4d4a377d15506d0ff070a7947d35b8466e2ee
hosuhosoo <hosuhosoo@gmail.com>
2022-01-21 18:02:23 +0900

    RacingTest 수정

--- src/test/java/racingTest/RacingTest.java
+++ src/test/java/racingTest/RacingTest.java
@@ -1,12 +1,12 @@
 package racingTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import racing.Car;
-import racing.RandomMove;
+import racing.RacingEvent;
 
 public class RacingTest {
     @Test
@@ -15,20 +15,23 @@ public class RacingTest {
         Car car = new Car();
     }
 
-    @ParameterizedTest
-    @DisplayName("car가 4 이상의 숫자만 전진하는 테스트")
-    @ValueSource(ints = {0, 1, 2, 3})
-    void doesCarProceed(int value) {
-        Car car = new Car();
-
-    }
-
     @Test
-    @DisplayName("숫자타입이 입력이 되는지를 테스트한다.")
-    void moveTest(){
-        RandomMove move = new RandomMove();
-        move.equals(1);
+    @DisplayName("차량 대수를 지정한 수 만큼 이벤트의 차량객체가 지정한 차량대수만큼 리스트에 추가되었는지 테스트한다")
+    void racingEventCarTest(){
+        int carCount = 3;
+        int tryCount = 4;
+        RacingEvent racingEvent = new RacingEvent(carCount,tryCount);
+        assertThat(racingEvent.getCarList().size()).isEqualTo(carCount);
     }
+
+/*    @Test
+    @DisplayName("차량마다 시도횟수 만큼 4이상의 랜덤값이 배정 되었는지 테스트한다.")
+    void racingEventTryTest(){
+        int carCount = 3;
+        int tryCount = 4;
+        RacingEvent racingEvent = new RacingEvent(carCount,tryCount);
+        assertThat(racingEvent.getCarList().size()).isEqualTo(carCount);
+    }*/
 
 }
 
