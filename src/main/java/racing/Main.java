cb29e2c3a80d398721dcfcacf7a2dcdf44928e35
hosuhosoo <hosuhosoo@gmail.com>
2022-01-27 10:21:10 +0900

    MAIN 파일 입니다.

--- src/main/java/racing/Main.java
+++ src/main/java/racing/Main.java
@@ -1,6 +1,13 @@
 package racing;
 
+import racing.domain.Cars;
+import racing.domain.RacingEvent;
+import racing.view.InputView;
+import racing.view.ResultView;
+
 public class Main {
+
+    private static final String RESULT_MEG = "실행결과";
 
     public static void main(String[] args) {
 
@@ -8,18 +15,24 @@ public class Main {
         int carCount = inputView.getCarCount();
         int tryCount = inputView.getTryCount();
         RacingEvent racingEvent = new RacingEvent(carCount,tryCount);
-        ResultView resultView = new ResultView();
+
+        System.out.println(RESULT_MEG);
 
         for(int i = 1; i <= tryCount; i++){
-            checkCars(racingEvent, resultView, i);
-            resultView.printResultSpace();
+            checkCars(racingEvent, i);
+            printResultSpace();
         }
     }
 
-    private static void checkCars(RacingEvent racingEvent, ResultView resultView, int i) {
+    private static void checkCars(RacingEvent racingEvent, int i) {
+        ResultView resultView = new ResultView();
         for(Cars cars : racingEvent.getCarList()){
             resultView.printResultPosition(cars.getPosition().get(i));
-
         }
+    }
+
+    private static void printResultSpace(){
+        System.out.println("");
+        System.out.println("");
     }
 }
